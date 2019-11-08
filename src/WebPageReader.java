763566d9c63950e6d165bc8a77cf277eef52e921
lixin000122 <56465590+lixin000122@users.noreply.github.com>
2019-11-08 22:04:37 +0800

    MCOTD
    create new program

---
+++ src/WebPageReader.java
@@ -0,0 +1,25 @@
+import java.net.*;
+import java.io.*;
+
+public class WebPageReader {
+
+	public static void main(String[] args) {		
+		try{
+			int n=-1;
+			URL url=new URL("https://www.baidu.com/");   
+			   
+			byte[] b = new byte[112];
+			StringBuffer area = new StringBuffer();
+			InputStream in = url.openStream();   //in为输入流，指向URL对象所包含的资源。
+			while((n=in.read(b))!= -1){
+			      String s= new String(b, 0, n);
+			      area.append(s+"\r\n");  			      
+			}
+			System.out.println(area.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
